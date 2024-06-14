efb420aa1940adbd6944482902e7e5dbf6d74e97
Harun <harunabdi8@gmail.com>
2024-06-14 15:00:45 +0200

    yesir

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   build: {
     outDir: "dist",
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: "src/main.jsx",
+    },
     // other build options
   },
   plugins: [react()],
