d579d0347545ad073ce723b1a344664584c3d1d1
Harun <harunabdi8@gmail.com>
2024-06-17 08:27:01 +0200

    mime types

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     outDir: "dist",
     chunkSizeWarningLimit: 1000, // Increase the chunk size limit to 1000 kB
-
+    base: "./",
     manifest: true,
     rollupOptions: {
       // overwrite default .html entry
