f44b603001ad0659b73b9920f0c4e05673f57fea
Harun <harunabdi8@gmail.com>
2024-06-19 14:04:52 +0200

    plzzzzz brooooo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // build: {
-  //   outDir: "dist",
-  //   chunkSizeWarningLimit: 1000, // Increase the chunk size limit to 1000 kB
-  //   base: "/blog/frontend/",
-  //   manifest: true,
-  //   rollupOptions: {
-  //     // overwrite default .html entry
-  //     input: "/src/main.jsx",
-  //   },
-  //   // other build options
-  // },
+  build: {
+    outDir: "dist",
+    chunkSizeWarningLimit: 1000, // Increase the chunk size limit to 1000 kB
+    base: "https://yellow-bush-0a5a05603.5.azurestaticapps.net/",
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: "/src/main.jsx",
+    },
+    // other build options
+  },
   plugins: [react()],
 });
